65cac7404aa23640826be98b23472f4ad25397d6
justinlulejian <justin@lulejian.com>
2017-03-15 21:44:10 -0700

    post lab1 stuff

---
+++ CSE110/week1/week1/src/week1/Lab1.java
@@ -0,0 +1,17 @@
+//***********************************************************
+// Name: Justin Lulejian
+// Title: Lab1.java
+// Description: Prints out My first program and my name.
+// Time spent: 10 minutes
+// Date: 3/15/2017
+//***********************************************************
+package week1;
+
+public class Lab1
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Hello, my name is Justin Lulejian.");
+		System.out.println("This is my first program!");
+	}
+}
